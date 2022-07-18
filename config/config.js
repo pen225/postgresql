0745241a5593441c0548176aa2f7a11c0fc48d98
Essoh Jean Penuel <penuel225@gmail.com>
2022-07-18 04:56:19 +0000

    dfghj

---
+++ config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+module.exports = {
+    database : process.env.DB_DATABASE,
+    username : process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: 5432,
+    ssl:true,
+    dialectOptions: {
+        ssl: {
+            require:true,
+            rejectUnauthorized: false
+        }
+    }
+  
+}
+  
